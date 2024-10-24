7e001e4197dd956e821e42ee561b79f2b5333849
Joshua Born <joshua.born@gmail.com>
2024-10-24 16:31:25 -0400

    Default paybacks to today's date

--- app/controllers/paybacks_controller.rb
+++ app/controllers/paybacks_controller.rb
@@ -1,6 +1,7 @@
 class PaybacksController < ApplicationController
   def new
     @payback = Payback.new
+    @payback.date = Date.today
     @person_transfers = @current_user.get_amounts_owed
     render layout: false
   end
