5b773a0c4ff7af0eec040eb78bbd72d99a169527
Joshua Born <joshua.born@gmail.com>
2024-10-31 13:37:00 -0400

    Send users to login page after logging out to avoid warning message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
     end
+
+  private
+    def after_sign_out_path_for(resource_or_scope)
+      new_person_session_path
+    end
 end
