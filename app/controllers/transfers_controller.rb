a97f502b629d6bebeae7b150cebaaae21c83acc2
Joshua Born <joshua.born@gmail.com>
2024-11-01 16:24:34 -0400

    Use "side panel" consistently and remove "side frame"

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,5 +1,5 @@
 class TransfersController < ApplicationController
-  layout "side_frame"
+  layout "side_panel"
 
   def index
     @person_transfers = current_person.person_transfers.includes(:transfer, :person_transfers, :people).order(transfers: { date: :desc })
