1dc6da29cbf613789e24bb5fd511ffce2a29dc3a
Joshua Born <joshua.born@gmail.com>
2024-11-01 10:33:00 -0400

    Revert "Set up a test mailer to try to get emails working in development environment"
    This reverts commit 82a975ae7f913c7e08b30ef21a8f5f40a64834f3.

--- app/mailers/test_mailer.rb
+++
@@ -1,8 +0,0 @@
-class TestMailer < ApplicationMailer
-  def test_email
-    mail(from: "joshua.born@gmail.com", to: "joshua.born@gmail.com", subject: "Test Email") do |format|
-      format.text { render plain: "This is a test email." }
-      format.html { render html: "<h1>This is a test email.</h1>".html_safe }
-    end
-  end
-end
