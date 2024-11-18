6f02e64d233cb37b43c8685ebe08af943bf445b9
Joshua Born <joshua.born@gmail.com>
2024-11-18 13:38:44 -0500

    Make welcome page scrollable

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_person!
-  layout "hero"
+  layout "plain"
 
   def index
     redirect_to transfers_path if person_signed_in?
