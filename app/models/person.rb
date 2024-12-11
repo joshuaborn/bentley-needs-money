4e732b1c5734f1efc4d824eef60e792d7520c182
Joshua Born <joshua.born@gmail.com>
2024-12-11 11:43:27 -0500

    Use associations for connection requests

--- app/models/person.rb
+++ app/models/person.rb
@@ -6,6 +6,8 @@ class Person < ApplicationRecord
   has_many :expenses, through: :person_transfers, source: :transfer
   has_many :paybacks, through: :person_transfers, source: :transfer
   has_many :signup_requests, inverse_of: "from"
+  has_many :outbound_connection_requests, class_name: "ConnectionRequest", inverse_of: "from"
+  has_many :inbound_connection_requests, class_name: "ConnectionRequest", inverse_of: "to"
   has_many :connections, inverse_of: "from"
   has_many :connected_people, through: :connections, source: :to
 
@@ -22,10 +24,10 @@ class Person < ApplicationRecord
       if preexisting_connection.present?
         preexisting_connection
       else
-        ConnectionRequest.create!(from: self, to: other_person)
+        self.outbound_connection_requests.create(to: other_person)
       end
     else
-      SignupRequest.create!(from: self, to: email)
+      self.signup_requests.create(to: email)
     end
   end
 end
