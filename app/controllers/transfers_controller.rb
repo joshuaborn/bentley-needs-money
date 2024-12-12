a7cacfce77c567a02c159db3aeed17583ba39650
Joshua Born <joshua.born@gmail.com>
2024-12-12 18:14:47 -0500

    Redirect new users to the connections index page

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -2,11 +2,20 @@ class TransfersController < ApplicationController
   layout "side_panel"
 
   def index
-    @person_transfers = current_person.person_transfers.
-      includes(:transfer, :person_transfers, :people).
-      order(transfers: { date: :desc, created_at: :asc })
-    if current_person.inbound_connection_requests.any?
-      flash.now[:info] = "You have one or more connection requests. Navigate to the <a href='#{url_for connections_path}' target='_top'>Connections page</a> to approve or deny connection requests.".html_safe
+    if current_person.person_transfers.empty? and current_person.connections.empty?
+      if current_person.inbound_connection_requests.any?
+        flash[:info] = "In order to begin, you need a connection with another person. You already have someone who has requested to connect with you, so you can accept the request to start splitting expenses."
+      else
+        flash[:info] = "In order to begin, you need a connection with another person. Request a connection so that you can start splitting expenses."
+      end
+      redirect_to connections_path
+    else
+      @person_transfers = current_person.person_transfers.
+        includes(:transfer, :person_transfers, :people).
+        order(transfers: { date: :desc, created_at: :asc })
+      if current_person.inbound_connection_requests.any?
+        flash.now[:info] = "You have one or more connection requests. Navigate to the <a href='#{url_for connections_path}' target='_top'>Connections page</a> to approve or deny connection requests.".html_safe
+      end
     end
   end
 end
--- test/controllers/transfers_controller_test.rb
+++ test/controllers/transfers_controller_test.rb
@@ -4,16 +4,33 @@ class TransfersControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
   setup do
     people(:user_one).confirm
-    sign_in people(:user_one)
+    people(:user_five).confirm
   end
   test "getting #index" do
+    build_expenses_for_tests()
+    sign_in people(:user_one)
     get transfers_path
     assert_response :success
   end
   test "flash message when there are connection requests to accept or deny" do
+    build_expenses_for_tests()
     ConnectionRequest.create(from: people(:user_two), to: people(:user_one))
+    sign_in people(:user_one)
     get transfers_path
     assert_response :success
     assert_match "You have one or more connection requests.", flash[:info]
+  end
+  test "redirect to connections index when there are no transfers, no connections, and no connection requests" do
+    sign_in people(:user_five)
+    get transfers_path
+    assert_redirected_to connections_path
+    assert_equal "In order to begin, you need a connection with another person. Request a connection so that you can start splitting expenses.", flash[:info]
+  end
+  test "redirect to connections index when there are no transfers and no connections, but a connection request" do
+    ConnectionRequest.create(from: people(:user_two), to: people(:user_five))
+    sign_in people(:user_five)
+    get transfers_path
+    assert_redirected_to connections_path
+    assert_equal "In order to begin, you need a connection with another person. You already have someone who has requested to connect with you, so you can accept the request to start splitting expenses.", flash[:info]
   end
 end
