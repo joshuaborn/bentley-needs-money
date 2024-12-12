b7a9e4eac5b9f05d88492072dfded5bfd1dfe9df
Joshua Born <joshua.born@gmail.com>
2024-12-12 14:38:01 -0500

    Include flash message on Expenses page when there are connection requests

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -2,6 +2,11 @@ class TransfersController < ApplicationController
   layout "side_panel"
 
   def index
-    @person_transfers = current_person.person_transfers.includes(:transfer, :person_transfers, :people).order(transfers: { date: :desc, created_at: :asc })
+    @person_transfers = current_person.person_transfers.
+      includes(:transfer, :person_transfers, :people).
+      order(transfers: { date: :desc, created_at: :asc })
+    if current_person.inbound_connection_requests.any?
+      flash.now[:info] = "You have one or more connection requests. Navigate to the <a href='#{url_for connections_path}' target='_top'>Connections page</a> to approve or deny connection requests.".html_safe
+    end
   end
 end
--- test/controllers/transfers_controller_test.rb
+++ test/controllers/transfers_controller_test.rb
@@ -4,10 +4,16 @@ class TransfersControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
   setup do
     people(:user_one).confirm
+    sign_in people(:user_one)
   end
   test "getting #index" do
-    sign_in people(:user_one)
     get transfers_path
     assert_response :success
+  end
+  test "flash message when there are connection requests to accept or deny" do
+    ConnectionRequest.create(from: people(:user_two), to: people(:user_one))
+    get transfers_path
+    assert_response :success
+    assert_match "You have one or more connection requests.", flash[:info]
   end
 end
