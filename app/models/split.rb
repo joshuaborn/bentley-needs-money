0f8bc3913314f80aaa4e85151ae943b3763109ba
Joshua Born <joshua.born@gmail.com>
2025-03-14 16:53:08 -0400

    Implement splitting between two people with new Split model

--- app/models/split.rb
+++ app/models/split.rb
@@ -1,3 +1,16 @@
 class Split < Reason
   validates_presence_of :payee, :amount
+
+  def self.between_two_people(payer, ower, attrs = {})
+    Split.new(attrs).tap do |split|
+      half_amount = split.amount.to_f / 2
+      if half_amount % 1 == 0 then
+        split.debts.new(owed: payer, ower: ower, amount: half_amount)
+      elsif rand() <= 0.5 then
+        split.debts.new(owed: payer, ower: ower, amount: half_amount.floor)
+      else
+        split.debts.new(owed: payer, ower: ower, amount: half_amount.ceil)
+      end
+    end
+  end
 end
---
+++ spec/models/split_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe Split, type: :model do
+  describe ".between_two_people" do
+    let(:payer) { FactoryBot.create(:person) }
+    let(:ower) { FactoryBot.create(:person) }
+
+    context "when amount paid is evenly divisible" do
+      subject(:split) do
+        attrs = FactoryBot.attributes_for(:split)
+        attrs[:amount] = 100
+        Split.between_two_people(payer, ower, attrs)
+      end
+
+      it "gives the ower half the split's amount as debt" do
+        expect(split.amount).to eq(100)
+        expect(split.debts.length).to eq(1)
+        expect(split.debts[0].amount).to eq(50)
+        expect(split.debts[0].ower).to eq(ower)
+        expect(split.debts[0].owed).to eq(payer)
+      end
+    end
+
+    context "when there is an odd cent out and rand() <= 0.5" do
+      subject(:split) do
+        attrs = FactoryBot.attributes_for(:split)
+        attrs[:amount] = 99
+        allow(Split).to receive(:rand).and_return(0.25)
+        Split.between_two_people(payer, ower, attrs)
+      end
+
+      it "gives the one cent discount to the ower" do
+        expect(split.amount).to eq(99)
+        expect(split.debts.length).to eq(1)
+        expect(split.debts[0].amount).to eq(49)
+        expect(split.debts[0].ower).to eq(ower)
+        expect(split.debts[0].owed).to eq(payer)
+      end
+    end
+
+    context "when there is an odd cent out and rand() > 0.5" do
+      subject(:split) do
+        attrs = FactoryBot.attributes_for(:split)
+        attrs[:amount] = 99
+        allow(Split).to receive(:rand).and_return(0.75)
+        Split.between_two_people(payer, ower, attrs)
+      end
+
+      it "gives the extra cent to the ower's debt" do
+        expect(split.amount).to eq(99)
+        expect(split.debts.length).to eq(1)
+        expect(split.debts[0].amount).to eq(50)
+        expect(split.debts[0].ower).to eq(ower)
+        expect(split.debts[0].owed).to eq(payer)
+      end
+    end
+  end
+end
