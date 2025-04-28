e0e3c0bfe610a76593f14fecd5be0a8d4e0048ff
Joshua Born <joshua.born@gmail.com>
2025-04-28 12:25:05 -0400

    Log requests rejected by invisible_captcha to BotAccountRequest model

--- app/controllers/people/registrations_controller.rb
+++ app/controllers/people/registrations_controller.rb
@@ -1,5 +1,5 @@
 class People::RegistrationsController < Devise::RegistrationsController
-  invisible_captcha only: [ :create ], scope: :person, honeypot: :username
+  invisible_captcha only: [ :create ], scope: :person, honeypot: :username, on_spam: :log_spam_attempt
 
   def create
     super do |person|
@@ -12,5 +12,15 @@ class People::RegistrationsController < Devise::RegistrationsController
   protected
     def after_inactive_sign_up_path_for(resource_or_scope)
       new_person_session_path
+    end
+
+  private
+    def log_spam_attempt
+      BotAccountRequest.create(
+        username: params["person"]["username"],
+        name: params["person"]["name"],
+        email: params["person"]["email"]
+      )
+      head :ok
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_25_182055) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_28_161830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "bot_account_requests", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "connection_requests", force: :cascade do |t|
     t.integer "from_id"
