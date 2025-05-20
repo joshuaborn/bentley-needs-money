11007783be2a1ccf1a1549b0ef434d9b33e1f578
Joshua Born <joshua.born@gmail.com>
2025-05-20 14:26:02 -0400

    Remove use of mock.module
    Mock.module calls cannot be undone. Therefore, if a module is mocked in
    any one test, this might break other tests.
    https://github.com/oven-sh/bun/issues/7823
    
    The test that relied on mock.module will be moved to another test when
    the corresponding code is moved to NewRepaymentCard, anyway.

--- spec/javascript/components/App.test.tsx
+++ spec/javascript/components/App.test.tsx
@@ -1,23 +1,11 @@
 import { render } from '@testing-library/react';
-import { describe, it, expect, mock, beforeEach } from "bun:test";
+import { describe, it, expect, beforeEach } from "bun:test";
 
-import { johnSmith, billyBob } from '../fixtures/people';
-import { debts } from '../fixtures/debts';
 import { mockAppProps } from '../fixtures/mockAppProps';
 import App from '../../../app/javascript/components/App';
-import type { Debt } from '../../../app/javascript/types';
-
-const mockSidePanel = mock();
-
-await mock.module('../../../app/javascript/components/SidePanel', () => {
-    return {
-        default: mockSidePanel
-    };
-});
 
 describe('App', () => {
     beforeEach(() => {
-        mockSidePanel.mockClear();
         render(<App {...mockAppProps} />);
     });
 
@@ -27,12 +15,12 @@ describe('App', () => {
     });
 
     // TODO: Move this to NewRepaymentCard.test.tsx.
-    it('passes a list of the most recent debt for each connected person to SidePanel as peopleOwed', () => {
-        const actualPeopleOwed = mockSidePanel.mock.calls[0][0].peopleOwed;
-        const johnSmithDebt = actualPeopleOwed.find((d: Debt) => d.person.id === johnSmith.id);
-        const billyBobDebt = actualPeopleOwed.find((d: Debt) => d.person.id === billyBob.id);
-        expect(johnSmithDebt).toEqual(debts[1]);
-        expect(billyBobDebt).toEqual(debts[4]);
-        expect(actualPeopleOwed.length).toBe(2);
-    });
+    // it('passes a list of the most recent debt for each connected person to SidePanel as peopleOwed', () => {
+    //     const actualPeopleOwed = mockSidePanel.mock.calls[0][0].peopleOwed;
+    //     const johnSmithDebt = actualPeopleOwed.find((d: Debt) => d.person.id === johnSmith.id);
+    //     const billyBobDebt = actualPeopleOwed.find((d: Debt) => d.person.id === billyBob.id);
+    //     expect(johnSmithDebt).toEqual(debts[1]);
+    //     expect(billyBobDebt).toEqual(debts[4]);
+    //     expect(actualPeopleOwed.length).toBe(2);
+    // });
 });
