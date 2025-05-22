807010de27c0d22072cc26425fcc6d401a7bf714
Joshua Born <joshua.born@gmail.com>
2025-05-22 10:28:47 -0400

    Only initialize Redis if environment variable is present

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,12 +1,14 @@
-redis_url = {
-  development: ENV.fetch("REDIS_URL", "redis://localhost:6379/0"),
-  test: ENV.fetch("REDIS_TEST_URL", "redis://localhost:6380/0"),
-  production: ENV.fetch("REDIS_URL")
-}
+if ENV.include?("REDIS_URL") or ENV.include?("REDIS_TEST_URL")
+  redis_url = {
+    development: ENV.fetch("REDIS_URL"),
+    test: ENV.fetch("REDIS_TEST_URL"),
+    production: ENV.fetch("REDIS_URL")
+  }
 
-$redis = ConnectionPool::Wrapper.new do
-  Redis.new(
-    url: redis_url[Rails.env.to_sym],
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
-  )
+  $redis = ConnectionPool::Wrapper.new do
+    Redis.new(
+      url: redis_url[Rails.env.to_sym],
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+    )
+  end
 end
